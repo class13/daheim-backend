e3591eddd26dae9048c6ed854c64e798f47bf954
Lukas Schneider <wiiinator1@gmail.com>
2017-01-17 01:42:07 +0100

    Implementing v002 requirements

--- daheim.rb
+++ daheim.rb
@@ -24,6 +24,18 @@ before do
   end
 end
 
+def validate_not_null(field_array)
+  field_array.each do |field_name|
+    unless @params.has_key?(field_name)
+      throw "parameter %s missing" % field_name
+    end
+  end
+end
+
+def return_error(error)
+  return json :success => false, :error => error
+end
+
 post '/create-user' do
   name = @params['name']
   uuid = ""
@@ -35,7 +47,55 @@ post '/create-user' do
   json :success => true, :uuid => uuid
 end
 
-post '/build-home' do
+post '/join-home' do
+  begin
+    validate_not_null ["pssid", "uuid"]
+  rescue Exception => e
+    return return_error e.ca
+  end
+  #init
   pssid = @params['pssid']
-  
+  uuid = @params['uuid']
+  home = Home.find_by({:pssid => pssid})
+  user = User.find_by({:uuid => uuid})
+  #validate
+  error = nil
+  if home.nil?
+    error = "entry home missing"
+  end
+  if user.nil?
+    error = "entry user missing"
+  end
+  unless error.nil?
+    return return_error error
+  end
+  #join
+  user.update({:home => home})
+  #response
+  return json :success => true, :home => home.id
+end
+
+post '/create-home' do
+  #request validation
+  begin
+    validate_not_null ['pssid', 'name', 'uuid']
+  rescue Exception => error
+    return return_error error.message
+  end
+  #init
+  pssid = @params['pssid']
+  name = @params['name']
+  uuid = @params['uuid']
+  user = User.find_by({:uuid => uuid})
+  #valdation
+  if user.nil?
+    return return_error "entry user missing"
+  end
+  #creation
+  home = Home.create({:pssid => pssid, :name => name})
+
+  #join user 2 group
+  user.update({:home => home})
+  #response
+  return json :success => true, :home => home.id
 end
