16c5c640baacd03fa8c4fec665749b70d3cfa2db
Lukas Schneider <wiiinator1@gmail.com>
2017-03-05 17:11:12 +0100

    db: user can now have multiple status the actual status is determined by a create_data and a expiring date fixed count of user in check-home

--- model.rb
+++ model.rb
@@ -8,10 +8,9 @@ class Home < ActiveRecord::Base
 end
 
 class HomeDetail < ActiveRecord::Base
-	self.table_name = "home_detail"
+	self.table_name = "v_home_detail"
 end
 
 class Memberstatus < ActiveRecord::Base
-	self.table_name = "USER_TO_HOME"
-	self.primary_key = "USER"
+	self.table_name = "v_user_home_status"
 end
